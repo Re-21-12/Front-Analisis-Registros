4c6cb36bf59e3a78f40b25161ccb1c1f50cbdf0f
Re-21-12 <victora.caas@gmail.com>
2025-05-21 16:41:03 -0600

    [FIX]: fix route

--- src/app/core/pages/login/login.component.ts
+++ src/app/core/pages/login/login.component.ts
@@ -1,21 +1,21 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { Component, DestroyRef, inject, OnInit } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatCheckboxModule } from "@angular/material/checkbox";
 import { DynamicFormComponent } from "../../shared/components/dynamic-form/dynamic-form.component";
-import { FormTemplateModel } from '../../shared/components/dynamic-form/models/form-template';
-import { Forms } from '../../shared/components/dynamic-form/models/form-list';
-import { Router } from '@angular/router';
-import { PersonaService } from '../../api/services/persona.service';
-import { PersonaResponse } from '../../shared/models/persona';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { LocalStorageService } from '../../services/local-storage.service';
+import { FormTemplateModel } from "../../shared/components/dynamic-form/models/form-template";
+import { Forms } from "../../shared/components/dynamic-form/models/form-list";
+import { Router } from "@angular/router";
+import { PersonaService } from "../../api/services/persona.service";
+import { PersonaResponse } from "../../shared/models/persona";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { LocalStorageService } from "../../services/local-storage.service";
 
 @Component({
-  selector: 'app-login',
+  selector: "app-login",
   standalone: true,
   imports: [
     MatButtonModule,
@@ -24,13 +24,12 @@ import { LocalStorageService } from '../../services/local-storage.service';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    DynamicFormComponent
+    DynamicFormComponent,
   ],
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-
   private _router = inject(Router);
   private _personService = inject(PersonaService);
   private _destroyRef = inject(DestroyRef);
@@ -47,20 +46,26 @@ export class LoginComponent implements OnInit {
   }
 
   listenSubmit($event: string): void {
-        const formData = JSON.parse($event)
-    const {id} = formData
+    const formData = JSON.parse($event);
+    const { id } = formData;
 
-    this._personService.getById(id).pipe(takeUntilDestroyed(this._destroyRef)).subscribe({
-      next: (response: PersonaResponse | null) => {
-        if(!response) return
-        this._localStorage.setItem('tipoPersonaNombre', JSON.stringify(response.tipoPersonaNombre));
-        this._localStorage.setItem('id', JSON.stringify(response.id));
-        this._router.navigate(['/personas']);
-      }
-    });
+    this._personService
+      .getById(id)
+      .pipe(takeUntilDestroyed(this._destroyRef))
+      .subscribe({
+        next: (response: PersonaResponse | null) => {
+          if (!response) return;
+          this._localStorage.setItem(
+            "tipoPersonaNombre",
+            JSON.stringify(response.tipoPersonaNombre),
+          );
+          this._localStorage.setItem("id", JSON.stringify(response.id));
+          this._router.navigate(["/personas"]);
+        },
+      });
   }
   preregister() {
-    this._router.navigate(['personas/new-persona'])
+    this._router.navigate(["personas/new-persona-by-user"]);
   }
 }
 // TODO: Falta colocar registro de registrador
